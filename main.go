cdcc1f12d5e0f48a254bb01a567a102aa604851d
Will Clarke <will.clarke@justeattakeaway.com>
2023-06-28 11:34:42 +0100

    Repl / main done - CHAPTER ONE DONE!!!!

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
